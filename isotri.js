2d6a9ae3d753b1e0541c7332ad1bcac440260df4
emanuel <ecastillo76@misena.edu.co>
2022-01-31 11:29:43 -0500

    Starting to design logic to draw

---
+++ isotri.js
@@ -0,0 +1,73 @@
+function draw() {
+    const canvas = document.getElementById("canvas");
+
+    if (!canvas.getContext) {
+        return;
+    }
+    const ctx = canvas.getContext("2d");
+
+    // set line stroke and line width
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 3;
+
+    // draw a red line
+    ctx.beginPath();
+    ctx.moveTo(100, 100);
+    ctx.lineTo(300, 100);
+    ctx.stroke();
+
+}
+
+
+function alturaIsosceles(lado1, lado2, lado3) {
+
+    let a, b, c, h;
+
+    if (lado1 === lado2 && lado1 === lado3){
+
+        console.log("Es Equilatero");
+        return "Es equilatero";
+
+    } else if (lado1 === lado2 && lado1 !== lado3) {
+        a = lado1;
+        b = lado2;
+        c = lado3;
+
+    } else if (lado1 === lado3 && lado1 !== lado2) {
+        a = lado1;
+        b = lado3;
+        c = lado2;
+
+    } else if (lado2 === lado3 && lado2 !== lado1) {
+        a = lado2;
+        b = lado3;
+        c = lado1;
+
+    } else {
+        console.log("Es Escaleno");
+        return null;
+    }
+
+    h = Math.sqrt(( Math.pow(a, 2) ) - ( ( Math.pow(c, 2) ) / 4 ));
+    if (h === 0) {
+        console.log("Los lados del triángulo no tienen lógica");
+    } else {
+        return h;
+        console.log(`Altura del triángulo Isosceles: ${h}`);
+    }
+}
+
+function calcularAltura() {
+    const lado1 = document.getElementById("lado1").value;
+    const lado2 = document.getElementById("lado2").value;
+    const lado3 = document.getElementById("lado3").value;
+
+    Number(lado1);
+    Number(lado2);
+    Number(lado3);
+
+    const altura = alturaIsosceles(lado1, lado2, lado3);
+    alert(altura);
+    draw();
+
+}
