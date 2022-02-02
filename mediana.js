707aaad301ed15f979dcdc9aabd1cded209527f8
emanuel <ecastillo76@misena.edu.co>
2022-02-02 07:28:35 -0500

    adding mediana solution

---
+++ mediana.js
@@ -0,0 +1,48 @@
+// Retos
+// 1. convertir todo a una función para usarlo con cualquier lista
+// 2. ordenar la lista con .sort
+function calcularMediaAritmetica(lista) {  
+
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento;
+        }
+    );    
+    const promedioLista = sumaLista / lista.length;
+    return promedioLista;
+};
+
+const lista1 = [
+    100,
+    200,
+    1400,
+    500,
+    400000000,
+];
+
+const mitadLista1 = parseInt(lista1.length / 2);
+
+function esPar(numerito) {
+    if (numerito % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let mediana;
+
+if (esPar(lista1.length)) {
+    const elemento1 = lista1[mitadLista1 - 1];
+    const elemento2 = lista1[mitadLista1];
+
+    const promedioElemento1y2 = calcularMediaAritmetica([
+        elemento1,
+        elemento2,
+    ])
+
+    mediana = promedioElemento1y2;
+    
+} else {
+    mediana = lista1[mitadLista1];
+}
