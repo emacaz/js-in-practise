3b3d3d155cc960ac0a7c6961272d5f0316155426
emanuel <ecastillo76@misena.edu.co>
2022-02-08 17:25:06 -0500

    Terminando curso

---
+++ salarios.js
@@ -0,0 +1,81 @@
+const colombia = [];
+colombia.push({
+    name: "Carlos",
+    salary: 500,
+});
+colombia.push({
+    name: "Juliana",
+    salary: 5000,
+});
+colombia.push({
+    name: "Adriana",
+    salary: 1000,
+});
+colombia.push({
+    name: "Carmen",
+    salary: 1500,
+});
+colombia.push({
+    name: "Karen",
+    salary: 800,
+});
+colombia.push({
+    name: "Azucena",
+    salary: 2000,
+});
+colombia.push({
+    name: "Nicole",
+    salary: 750,
+});
+colombia.push({
+    name: "Pamela",
+    salary: 3000,
+});
+colombia.push({
+    name: "Luisa",
+    salary: 900,
+});
+colombia.push({
+    name: "Valentina",
+    salary: 550,
+});
+colombia.push({
+    name: "Inés",
+    salary: 500,
+});
+colombia.push({
+    name: "Arnulfo",
+    salary: 1100,
+});
+colombia.push({
+    name: "Guadalupe",
+    salary: 1300,
+});
+colombia.push({
+    name: "Cynthia",
+    salary: 1800,
+});
+colombia.push({
+    name: "Lorena",
+    salary: 990,
+});
+colombia.push({
+    name: "Andrea",
+    salary: 1450,
+});
+colombia.push({
+    name: "Marcela",
+    salary: 4000,
+});
+colombia.push({
+    name: "Ariana",
+    salary: 3500,
+});
+colombia.push({
+    name: "Esteban",
+    salary: 4800,
+});
+colombia.push({
+    name: "Daniel",
+    salary: 770,
+});
